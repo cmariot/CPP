f337793a41760777c62e9db7b509c0fd9a6a4a63
Charles Mariot <contact@charles-mariot.fr>
2022-02-15 14:47:09 +0100

    Empty input [15 févr. 2022 à 14:46]

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 13:01:04 by cmariot           #+#    #+#             */
-/*   Updated: 2022/02/15 13:57:03 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/02/15 14:43:55 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,18 +46,29 @@ int	check_if_valid_index(std::string input)
 	return (0);
 }
 
-void	ask_for_contact_info(t_input *contact_input)
+int	ask_for_contact_info(t_input *contact_input)
 {
 	std::cout << "First name: ";
 	std::cin >> contact_input->first_name;
+	if (!contact_input->first_name[0])
+		return (1);
 	std::cout << "Last name: ";
 	std::cin >> contact_input->last_name;
+	if (!contact_input->last_name[0])
+		return (1);
 	std::cout << "Nickname: ";
 	std::cin >> contact_input->nickname;
+	if (!contact_input->nickname[0])
+		return (1);
 	std::cout << "Phone number: ";
 	std::cin >> contact_input->phone_number;
+	if (!contact_input->phone_number[0])
+		return (1);
 	std::cout << "Darkest secret: ";
 	std::cin >> contact_input->darkest_secret;
+	if (!contact_input->darkest_secret[0])
+		return (1);
+	return (0);
 }
 
 int	main(void)
@@ -72,18 +83,30 @@ int	main(void)
 	{
 		std::cout << "Type a command ('ADD', 'SEARCH' or 'EXIT') :" << std::endl;
 		std::cin >> input;
+		if (input.empty() == true)
+		{
+			std::cout << "Error, input required" << std::endl;
+			return (1);
+		}
 		if (input.compare("ADD") == 0)
 		{
-			ask_for_contact_info(&contact_input);
-			Contact	new_contact(contact_index, contact_input.first_name, contact_input.last_name,
-				contact_input.nickname, contact_input.phone_number, contact_input.darkest_secret);
-			repertoire.add(new_contact, contact_index);
-			if (number_of_contacts != 8)
-				number_of_contacts++;
-			if (contact_index != 7)
-				contact_index++;
+			if (ask_for_contact_info(&contact_input) == 0)
+			{
+				Contact	new_contact(contact_index, contact_input.first_name, contact_input.last_name,
+					contact_input.nickname, contact_input.phone_number, contact_input.darkest_secret);
+				repertoire.add(new_contact, contact_index);
+				if (number_of_contacts != 8)
+					number_of_contacts++;
+				if (contact_index != 7)
+					contact_index++;
+				else
+					contact_index = 0;
+			}
 			else
-				contact_index = 0;
+			{
+				std::cout << "Error, a contact could not have empty informations" << std::endl;
+				return (1);
+			}
 		}
 		else if (input.compare("SEARCH") == 0)
 		{
@@ -97,6 +120,7 @@ int	main(void)
 		}
 		else if (input.compare("EXIT") == 0)
 			break ;
+		input.clear();
 	}
 	return (0);
 }
