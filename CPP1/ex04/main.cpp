68858b07580271dd75b67b3c1e7dbb2015761c88
Charles MARIOT <contact@charles-mariot.fr>
2022-03-07 16:51:53 +0100

    OK

--- CPP1/ex04/main.cpp
+++ CPP1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 08:31:41 by cmariot           #+#    #+#             */
-/*   Updated: 2022/03/07 08:48:56 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/03/07 15:56:19 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,41 @@
 
 int main(int argc, char **argv)
 {
-	std::string str1;
-	std::string str2;
-	std::ifstream	input(argv[3]);
+	std::string		ifs_name;
+	std::ifstream	ifs;
 
+	std::string		ofs_name;
+	std::ofstream	ofs;
+	
 	if (argc == 4)
 	{
+
+		//Open the file filename, check if the file exists
+		ifs.open(argv[1], std::ifstream::in);
+		if (!ifs.good())
+		{
+			std::cout << "Error: no such file. Check the first argument." << std::endl;
+			return (1);
+		}
+
+		//Create and open the new file (filename.replace)
+		ifs_name = argv[1];
+		ofs_name = ifs_name.append(".replace");
+		char	*c = const_cast <char *> (ofs_name.c_str());
+		ofs.open (c, std::ofstream::out | std::ofstream::app);
+		if (!ofs.good())
+		{
+			std::cout << "Error: filename.replace could not be create." << std::endl;
+			ifs.close();
+			return (1);
+		}
+
+
+		
+
+
+		ofs.close();
+		ifs.close();
 
 		return (0);
 	}
