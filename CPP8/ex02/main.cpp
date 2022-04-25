daf2feb3639dc5852ef96c2b718f0a35750cd3a3
Charles Mariot <contact@charles-mariot.fr>
2022-04-25 13:17:59 +0200

    CPP

--- CPP8/ex02/main.cpp
+++ CPP8/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 12:06:39 by cmariot           #+#    #+#             */
-/*   Updated: 2022/04/07 13:31:11 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/04/25 12:38:25 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ void test_on_mutantStack(void)
 {
 	MutantStack<int> mstack;
 
+	std::cout << mstack.empty() << std::endl;
+
 	mstack.push(5);
 	mstack.push(17);
 
 	std::cout << mstack.top() << std::endl;
-	
+
 	mstack.pop();
 	
 	std::cout << mstack.size() << std::endl;
@@ -29,7 +31,8 @@ void test_on_mutantStack(void)
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...] mstack.push(0);
+	//[...]
+	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -45,28 +48,33 @@ void test_on_mutantStack(void)
 	
 	std::stack<int> s(mstack);
 
+	std::cout << (s == mstack) << std::endl;
+
 }
 
 void test_on_list(void)
 {
-	std::list<int> mstack;
-
-	mstack.push_back(5);
-	mstack.push_back(17);
-
-	std::cout << mstack.front() << std::endl;
+	std::list<int> mlist;
 	
-	mstack.pop_front();
-	
-	std::cout << mstack.size() << std::endl;
-	
-	mstack.push_back(3);
-	mstack.push_back(5);
-	mstack.push_back(737);
-	//[...] mstack.push(0);
+	std::cout << mlist.empty() << std::endl;
 
-	std::list<int>::iterator it = mstack.begin();
-	std::list<int>::iterator ite = mstack.end();
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+	
+	mlist.pop_back();
+	
+	std::cout << mlist.size() << std::endl;
+	
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	//[...]
+	mlist.push_back(0);
+
+	std::list<int>::iterator it = mlist.begin();
+	std::list<int>::iterator ite = mlist.end();
 
 	++it;
 	--it;
@@ -77,12 +85,16 @@ void test_on_list(void)
 		++it;
 	}
 	
-	std::list<int> s(mstack);
+	std::list<int> s(mlist);
+
+	std::cout << (s == mlist) << std::endl;
+
 }
 
 int main(void)
 {
 	test_on_mutantStack();
-	//test_on_list();
+	std::cout << "\n VS \n" << std::endl;
+	test_on_list();
 	return 0;
 }
